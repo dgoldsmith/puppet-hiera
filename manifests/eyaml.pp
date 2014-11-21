973e0d93a7e12712b01ab06f4c90341d7de3fa6e
dgoldsmith <dgoldsmith@sans.org>
2014-11-21 13:41:07 -0500

    Add parameters to provide content for public/private keys

--- manifests/eyaml.pp
+++ manifests/eyaml.pp
@@ -12,6 +12,8 @@
 #
 class hiera::eyaml (
   $provider = $hiera::params::provider,
+  $private_key_content = undef,
+  $public_key_content  = undef,
   $owner    = $hiera::owner,
   $group    = $hiera::group,
   $cmdpath  = $hiera::cmdpath,
@@ -30,29 +32,49 @@ class hiera::eyaml (
     before => Exec['createkeys'],
   }
 
-  exec { 'createkeys':
-    user    => $owner,
-    cwd     => $confdir,
-    command => 'eyaml createkeys',
-    path    => $cmdpath,
-    creates => "${confdir}/keys/private_key.pkcs7.pem",
-    require => Package['hiera-eyaml'],
-  }
+  if $private_key_content {
+      file { "${confdir}/keys/private_key.pkcs7.pem":
+        ensure  => file,
+        content => $private_key_content,
+        mode    => '0400',
+        owner   => $owner,
+        group   => $group,
+        require => File["${confdir}/keys"],
+      }
+      if $public_key_content {
+        file { "${confdir}/keys/public_key.pkcs7.pem":
+          ensure  => file,
+          content => $public_key_content,
+          mode    => '0400',
+          owner   => $owner,
+          group   => $group,
+          require => File["${confdir}/keys"],
+        }
+      }
+  } else {
+    exec { 'createkeys':
+      user    => $owner,
+      cwd     => $confdir,
+      command => 'eyaml createkeys',
+      path    => $cmdpath,
+      creates => "${confdir}/keys/private_key.pkcs7.pem",
+      require => Package['hiera-eyaml'],
+    }
 
+    file { "${confdir}/keys/private_key.pkcs7.pem":
+      ensure  => file,
+      mode    => '0600',
+      owner   => $owner,
+      group   => $group,
+      require => Exec['createkeys'],
+    }
 
-  file { "${confdir}/keys/private_key.pkcs7.pem":
-    ensure  => file,
-    mode    => '0600',
-    owner   => $owner,
-    group   => $group,
-    require => Exec['createkeys'],
-  }
-
-  file { "${confdir}/keys/public_key.pkcs7.pem":
-    ensure  => file,
-    mode    => '0644',
-    owner   => $owner,
-    group   => $group,
-    require => Exec['createkeys'],
+    file { "${confdir}/keys/public_key.pkcs7.pem":
+      ensure  => file,
+      mode    => '0644',
+      owner   => $owner,
+      group   => $group,
+      require => Exec['createkeys'],
+    }
   }
 }
